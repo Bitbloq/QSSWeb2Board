6459fdce6a4a243d2954e6a5996dd2506ca96859
Alberto Valero <alberto.valero@bq.com>
2018-04-15 19:54:47 +0200

    cleaning code

--- src/QSSWeb2Board/main.cpp
+++ src/QSSWeb2Board/main.cpp
@@ -172,7 +172,6 @@ int main(int argc, char *argv[])
 
     QSystemTrayIcon trayIcon(&a);
     trayIcon.setIcon(QIcon(QCoreApplication::applicationDirPath() + "/res/Web2board.ico"));
-    qDebug() << QCoreApplication::applicationDirPath() + "res/Web2board.ico";
     trayIcon.setToolTip("Web2Board");
 
     QMenu * menu = new QMenu();
