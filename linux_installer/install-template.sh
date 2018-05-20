091d98636b926c8152500502bfb71377fac79773
Alberto Valero <alberto.valero@bq.com>
2018-05-20 18:28:56 +0200

    new installer

---
+++ linux_installer/install-template.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# script for QSSWeb2Board
+
+echo "Vas a instalar Web2Board en tu sistema".
+echo "Has escogido realizar la instalación para ###OS### ###VERSION### ###ARCH### bits"
+while true; do
+    read -p "¿Es este tu sistema operativo? (SI/NO)" yn
+    case $yn in
+        [SI]* ) break;;
+        [NO]* ) exit;;
+        * ) echo "Por favor, contesta SI o NO";;
+    esac
+done
+
+#install dependencies
+sudo apt-get update
+
+sudo apt-get install qt5-default libqt5serialport5 libqt5websockets5 libqt5network5 libqt5widgets5 libqt5gui5 libqt5core5a
+
+###INSTALL_COMMAND###
+
+#CLEAR SCREEN
+#printf "\033c"
+echo ""
+echo ""
+echo ""
+echo "====== INSTALACIÓN FINALIZADA ======"
+echo "COMPRUEBE QUE NO HAY NINGÚN MENSAJE DE ERROR"
+echo "===================================="
+echo "===================================="
+echo "Si desea que otros usuarios usen Bitbloq deberán ser añadidos a los grupos qssweb2board y dialout"
+echo "adduser USUARIO qssweb2board"
+echo "adduser USUARIO dialout"
+echo "===================================="
+echo "Por favor, reincie el sistema"
---
+++ linux_installer/qssweb2board_2.0-template/etc/profile.d/10-qssweb2board.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export PATH=$PATH:/opt/QSSWeb2Board/
+export QSSWEB2BOARD_SKETCHES=/var/tmp/QSSWeb2Board/sketches/
+export QSSWEB2BOARD_BUILD=/var/tmp/QSSWeb2Board/build/
+export QSSWEB2BOARD_KNOWNBOARDS=/etc/QSSWeb2Board/knownboards.json
+export QSSWEB2BOARD_ARDUINO=/opt/QSSWeb2Board/res/arduino/
