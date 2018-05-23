7f6f15a42fc63e7638a2d28b7865d985aa661a36
Alberto Valero <alberto.valero@bq.com>
2018-05-23 09:57:18 +0200

    trying to register from installer

---
+++ win_installer/packages/com.vendor.root/meta/installscript.js
@@ -0,0 +1,11 @@
+Component.prototype.createOperations = function(){
+	try{
+		// call the base create operations function
+		component.createOperations();
+		if (installer.value("os") === "win"){
+		component.addOperation("Execute", "cmd /C regedit.exe /S "@TargetDir@\res\QSSWeb2Board.reg"");
+		}
+	} catch (e){
+		print(e);
+	}
+}
