5bb88216abb61a830edbb8418fbe1d19163e0787
Alberto Valero <alberto.valero@bq.com>
2018-05-23 13:50:57 +0200

    hide targetDir page

--- win_installer/config/controller.js
+++ win_installer/config/controller.js
@@ -3,7 +3,7 @@ function Controller()
     if ( installer.isInstaller())
     {
         installer.setDefaultPageVisible(QInstaller.ComponentSelection, false);
-        installer.setDefaultPageVisible(QInstaller.TargetDirectory, false);
+        //installer.setDefaultPageVisible(QInstaller.TargetDirectory, false);
     }
 
     installer.setMessageBoxAutomaticAnswer("AuthorizationError", QMessageBox.Abort); // Do not show AuthorizationError default window
@@ -30,6 +30,7 @@ Controller.prototype.TargetDirectoryPageCallback = function()
 {
     var currentPage = gui.currentPageWidget();
     currentPage.TargetDirectoryLineEdit.enabled = false;
+    currentPage.buttons[0].visible = false;
     //currentPage.TargetDirectoryLineEdit.textChanged.connect(this, Controller.prototype.targetChanged);
     //currentPage.MessageLabel.text = "It is highly recommended to install on C:\QSSWeb2Board:";
     //Controller.prototype.targetChanged(currentPage.TargetDirectoryLineEdit.text);
