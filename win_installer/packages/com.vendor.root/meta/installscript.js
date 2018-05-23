de44a2d431a47a0c19ce7cc618b00ef3bca9d80b
Alberto Valero <alberto.valero@bq.com>
2018-05-23 10:22:01 +0200

    ...

--- win_installer/packages/com.vendor.root/meta/installscript.js
+++ win_installer/packages/com.vendor.root/meta/installscript.js
@@ -3,7 +3,7 @@ Component.prototype.createOperations = function(){
 		// call the base create operations function
 		component.createOperations();
 		if (installer.value("os") === "win"){
-		component.addOperation("Execute", "cmd /C regedit.exe /S "@TargetDir@\res\QSSWeb2Board.reg"");
+			component.addOperation("Execute", "cmd /C regedit.exe /S " @TargetDir@\res\QSSWeb2Board.reg"");
 		}
 	} catch (e){
 		print(e);
