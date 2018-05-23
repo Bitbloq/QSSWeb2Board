32b6a6f4ee2484596359b13379b56c46604d21d1
Alberto Valero <alberto.valero@bq.com>
2018-05-23 13:00:06 +0200

    added icons to installer

--- win_installer/config/controller.js
+++ win_installer/config/controller.js
@@ -28,23 +28,23 @@ Controller.prototype.IntroductionPageCallback = function()
 Controller.prototype.TargetDirectoryPageCallback = function()
 {
     var currentPage = gui.currentPageWidget();
-    currentPage.TargetDirectoryLineEdit.disabled = true;
-    //currentPage.TargetDirectoryLineEdit.textChanged.connect(this, Controller.prototype.targetChanged);
-    //currentPage.MessageLabel.text = "Select an installation folder:";
-    //Controller.prototype.targetChanged(currentPage.TargetDirectoryLineEdit.text);
+    currentPage.TargetDirectoryLineEdit.enabled = false;
+    currentPage.TargetDirectoryLineEdit.textChanged.connect(this, Controller.prototype.targetChanged);
+    currentPage.MessageLabel.text = "It is highly recommended to install on C:\QSSWeb2Board:";
+    Controller.prototype.targetChanged(currentPage.TargetDirectoryLineEdit.text);
 }
 
 Controller.prototype.targetChanged = function (text)
 {
     var currentPage = gui.currentPageWidget();
 
-    if (text != "" && installer.fileExists(text + "/components.xml"))
+    if (text != "" && installer.fileExists(text + "/QSSWeb2Board.exe"))
     {
         currentPage.WarningLabel.text = "\n\tThis folder already contains an installation.\n\n\tPlease uninstall any previous version of Zetup before installing a new one.";
     }
     else
     {
-        currentPage.WarningLabel.text = "";
+        currentPage.WarningLabel.text = "\n\tIt is highly recommeded to install on C:\QSSWeb2Board.\n\n\tChosing another directory may cause malfunctioning.";
     }
 }
 
